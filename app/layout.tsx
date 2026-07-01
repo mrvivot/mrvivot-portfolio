fd39902b1207e9c039bddc7cc68ae24c50fc9b2e
Manuel Rojo Vivot <mrvivot@MacBook-Air-de-Manuel.local>
2026-07-01 16:02:21 -0300

    Add Microsoft Clarity analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import Script from 'next/script'
 import { ThemeProvider } from "@/lib/ThemeContext";
 import { LanguageProvider } from "@/lib/LanguageContext";
 import Nav from "@/components/layout/Nav";
@@ -39,6 +40,16 @@ export default function RootLayout({
             <Footer />
           </LanguageProvider>
         </ThemeProvider>
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+        >
+          {`(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "xfnpppw2mh");`}
+        </Script>
       </body>
     </html>
   );
